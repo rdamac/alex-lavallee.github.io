d88df8d56cf4c433a07f796c0a82cc45af9defa9
alex-lavallee <42420263+alex-lavallee@users.noreply.github.com>
2019-05-26 14:15:27 -0700

    Add files via upload

--- svg_test_1.js
+++ svg_test_1.js
@@ -1,7 +1,3 @@
-SVG.on(document, 'DOMContentLoaded', function() {
-  var draw = SVG('drawing')
-});
-
 if (SVG.supported) {
   var draw = SVG('drawing').size(300,300);
   var rect = draw.rect(100, 100).attr({ fill: '#f06' });
